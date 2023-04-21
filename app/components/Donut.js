f9642ef072fcce0cedcc98354faea4844f1aea50
Adam <chillcoders@gmail.com>
2023-04-21 17:57:39 -0400

    three-issues

--- app/components/Donut.js
+++ app/components/Donut.js
@@ -1,59 +1,92 @@
-import { Canvas } from "@react-three/fiber";
-import { useState } from "react";
-import { useSpring, a } from "@react-spring/three";
-import CameraControls from "./CameraControls";
+import React, { useRef, useEffect } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const Donut = () => {
-  const [hovered, setHover] = useState(false);
+  const mount = useRef(null);
 
-  const { color } = useSpring({
-    color: hovered ? "hotpink" : "gray",
-  });
+  useEffect(() => {
+    const width = mount.current.clientWidth;
+    const height = mount.current.clientHeight;
 
-  return (
-    <a.mesh
-      onPointerOver={() => setHover(true)}
-      onPointerOut={() => setHover(false)}
-    >
-      <torusGeometry args={[1, 0.5, 16, 100]} />
-      <a.meshStandardMaterial color={color} />
-    </a.mesh>
-  );
+    // Create the scene
+    const scene = new THREE.Scene();
+
+    // Create the camera
+    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    camera.position.set(0, 0, 10);
+
+    // Create the renderer
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setClearColor("#000000");
+    renderer.setSize(width, height);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    mount.current.appendChild(renderer.domElement);
+
+    // Add lighting
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    directionalLight.position.set(0, 1, 0);
+    scene.add(directionalLight);
+
+    // Create the donut
+    const geometry = new THREE.TorusGeometry(1, 0.5, 16, 100);
+    const material = new THREE.MeshStandardMaterial({ color: "#FF6347" });
+    const donut = new THREE.Mesh(geometry, material);
+    scene.add(donut);
+
+    // Create the rainbow
+    const rainbow = new THREE.Line(
+      new THREE.BufferGeometry().setFromPoints([
+        new THREE.Vector3(-10, -5, 0),
+        new THREE.Vector3(10, -5, 0),
+      ]),
+      new THREE.LineBasicMaterial({ color: "#FF0000" })
+    );
+    scene.add(rainbow);
+
+    // Add orbit controls
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.1;
+    controls.rotateSpeed = 0.5;
+    controls.autoRotate = true;
+    controls.autoRotateSpeed = 0.3;
+
+    // Render the scene
+    const animate = () => {
+      requestAnimationFrame(animate);
+
+      // Rotate the donut
+      donut.rotation.x += 0.01;
+      donut.rotation.y += 0.01;
+
+      // Animate the rainbow
+      const time = Date.now() * 0.0005;
+      const position = rainbow.geometry.attributes.position;
+      for (let i = 0; i < position.count; i++) {
+        const y = Math.sin(i / 2 + time);
+        position.setY(i, y);
+      }
+      position.needsUpdate = true;
+
+      // Update the controls
+      controls.update();
+
+      renderer.render(scene, camera);
+    };
+    animate();
+
+    return () => {
+      // Clean up the scene and renderer
+      scene.remove(donut);
+      scene.remove(rainbow);
+      renderer.dispose();
+    };
+  }, []);
+
+  return <div style={{ width: "100%", height: "100%" }} ref={mount} />;
 };
 
-const Rainbow = () => {
-  const colors = [
-    "red",
-    "orange",
-    "yellow",
-    "green",
-    "blue",
-    "indigo",
-    "violet",
-  ];
-
-  return (
-    <group>
-      {colors.map((color, index) => (
-        <mesh key={color} position={[0, -index * 0.1, 0]}>
-          <boxBufferGeometry args={[1, 0.1, 1]} />
-          <meshStandardMaterial color={color} />
-        </mesh>
-      ))}
-    </group>
-  );
-};
-
-const Scene = () => {
-  return (
-    <Canvas camera={{ position: [0, 0, 5] }}>
-      <CameraControls />
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 15, 10]} angle={0.3} />
-      <Rainbow />
-      <Donut />
-    </Canvas>
-  );
-};
-
-export default Scene;
+export default Donut;
