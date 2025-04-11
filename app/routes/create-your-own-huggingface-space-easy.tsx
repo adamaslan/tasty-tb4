7374422d48062e034547fe206132a60bec270926
adamaslan <chillcoders@gmail.com>
2025-04-11 18:34:55 -0400

    style

--- app/routes/create-your-own-huggingface-space-easy.tsx
+++ app/routes/create-your-own-huggingface-space-easy.tsx
@@ -37,32 +37,23 @@ export default function Article5() {
       
           <br />{" "}
 
-          <p className='text-left font-serif text-lg font-bold tracking-tight sm:text-2xl lg:text-3xl'>Hugging Face Spaces offer a simple way to host not just ML demo apps directly on your profile or your organization’s profile, but really any Python code you want. This is especially helpful for apps that use  
-
-           
-<a href="https://www.gradio.app/" >Gradio</a> Streamlit, Docker, or static HTML. </p> 
+          <p className='text-left font-serif text-lg font-bold tracking-tight sm:text-2xl lg:text-3xl'>Hugging Face Spaces offer a simple way to host not just ML demo apps directly on your profile or your organization’s profile, but really any Python code you want. <br /> </p><p className='text-left font-serif text-lg font-bold tracking-tight sm:text-2xl lg:text-3xl'>This is especially helpful for apps that use  
+     
+<a href="https://www.gradio.app/" className="text-blue-500"> Gradio</a>, Streamlit, Docker, or static HTML. </p> <br /> <br /> 
 
 <p className="text-left font-serif text-lg font-bold tracking-tight sm:text-2xl lg:text-3xl">
 Checkout <a href="https://huggingface.co/spaces/" className="text-blue-500">Hugging Face Spaces</a>
+<br />
 Here's there
  <a href="https://www.huggingface.co/" className="text-blue-500">
               {" "}
 docs for spaces
-            </a>Enjoy</p>
+            </a>
+      
 
 
           <br />{" "}
-          <p className="text-left font-serif text-lg font-bold tracking-tight sm:text-2xl lg:text-3xl">
-            {" "}
-            Whether you are purchasing a rare whiskey or an entire cask, the returns on these items have a clear track record of appreciation. <br /><br />For more information on Connecticut Distilling visit their website <a href="https://www.ctdistillingco.com/" className="text-blue-500"> here</a>.
-
-            <br /><br />For more information on Block Apps visit their website <a href="https://blockapps.net/" className="text-blue-500"> here</a>.
-
-            
-          
-
-{" "}
-          </p>{" "}
+      
           <p className="text-center text-lg font-extrabold tracking-tight text-yellow-500 sm:text-2xl lg:text-4xl">
             {" "}
             Go back{" "}
